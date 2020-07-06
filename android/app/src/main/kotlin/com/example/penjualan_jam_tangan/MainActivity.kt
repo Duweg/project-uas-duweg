3cba234e4c788958fed4ffd84d521906c586cb99
duweg <duwegjayasentana1234@gmail.com>
2020-07-07 00:22:37 +0800

    project UAS

---
+++ android/app/src/main/kotlin/com/example/penjualan_jam_tangan/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.penjualan_jam_tangan
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
